8fe5ace327f6dcbf60366746674bd0f254391157
Cook <dcook@email.neit.edu>
2022-05-21 14:03:21 -0400

    added dark and light theme switch capability

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import React from 'react';
-import {Link, Outlet} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import {Outlet} from 'react-router-dom';
+import Main from './components/Main';
+import { ThemeContext, themes } from './context/themeContext';
+import { useState, useContext } from 'react';
+import Navbar from './components/Navbar'
+import Button from './components/Button'
 
-function App() {
+function Providers() {
+  const [theme, setTheme] = useState(themes.dark);
 
+  const toggleTheme = () => {
+    setTheme((prevValue) =>
+      prevValue === themes.dark ? themes.light : themes.dark
+    );
+  };
   return (
-    <>
-      <nav className="navbar navbar-light navbar-expand-md bg-light px-2 m-1 border border-rounded">
-        <Link className='navbar-brand' to="/">Site Brand</Link>
-        <div className='container-fluid'>
-          <div className="navbar-nav">
-            <Link className='nav-link' to="/Products">Products</Link>
-            <Link className='nav-link' to="/About">About</Link>
-          </div>
-        </div>
-      </nav>
-      <div className='container'>
-        <Outlet />
-      </div>
-      
-      <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div className="col-md-4 d-flex align-items-center">
-            <Link className='text-decoration-none' to="/">Retailer Site Name</Link>
-            <span className="text-muted ms-1">| © 2022 Dylan Cook</span>
-          </div>
-        </footer>
-      </div>
-    </>
+    <ThemeContext.Provider value={{theme: theme}}>
+      <Navbar toggleTheme={toggleTheme}/>
+      <Main />
+    </ThemeContext.Provider>
   );
 }
 
-export default App;
+export default Providers;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import useFetch from '../hooks/useFetch.js';
 import Loading from './Loading.js'
 import ProductHeader from './ProductHeader';
 import Box from './Box'
 import {Link} from 'react-router-dom'
 import {useParams} from 'react-router';
+import { ThemeContext } from '../context/themeContext'
 
 const Products = () => {
-    //Check if urlFrag param pass works
     const {data, loading, error} = useFetch('products');
     const {productId} = useParams();
+    const { theme } = useContext(ThemeContext);
 
     if (loading){
         return (<Loading/>)
@@ -17,26 +18,32 @@ const Products = () => {
     if(error !== ''){
         return ({error})
     }
+
+    //Product Detail View
     if(productId !== undefined){
         const prod = data.find(prod => prod.id == productId);
         return (
-            <div className='d-flex'>
-                {<img src={prod.image} className="col-2"></img>}
-                <div className='d-flex flex-row col-8'>
+            <div className='d-flex justify-content-center'>
+                <div className='d-flex flex-row p-5' style={{color: theme.textColor}}>
+                {<img src={prod.image} className="col-3 me-2"></img>}
                     <div className='ms-2 d-flex flex-column'>
                         <h3>{prod.title}</h3>
                         <p>{prod.description}</p>
-                        <h4 className='align-self-end'>${prod.price}</h4>
-                        <div className='d-flex flex-column align-items-center align-self-end border border-rounded bg-light'>
-                            <h4>Rating</h4>
-                            <span>Rated <b>{prod.rating.rate}</b> out of a total of {prod.rating.count} reviews</span>
+                        <div className='align-self-end'>
+                            <h4>${prod.price}</h4>
+                            <div className='d-flex flex-column align-items-center border border-rounded p-2 my-4' style={{backgroundColor: theme.foreground}}>
+                                <h4>Rating</h4>
+                                <span>Rated <b>{prod.rating.rate}</b> out of a total of {prod.rating.count} reviews</span>
+                            </div>
+                            <Link to="./" className='mt-5' className='col-4'><button className='btn btn-primary'>Go Back to Products View</button></Link>
                         </div>
-                        <Link to="./" className='mt-5'>Go Back to Products View</Link>
                     </div>
                 </div>
             </div>
         )
     }
+
+    //Full products list
     return (
         <>
             {data.length && data.map((data) => {
@@ -44,14 +51,13 @@ const Products = () => {
                     <Box key={data.id} size='xl' children={
                         <div className='d-flex flex-row'>
                             {<img src={data.image} className="col-2"></img>}
-                            <div className='ms-2'>
+                            <div className='d-flex flex-column'>
                                 <ProductHeader productName={data.title} price={data.price} detailsLink />
                                 <p>{data.description}</p>
-                                <Link className='mx-auto' to={`./${data.id}`}>See Details</Link>
+                                <Link to={`./${data.id}`} className="align-self-end col-2"><button class='btn btn-primary'>See Details</button></Link>
                             </div>
                         </div>
-                    }>
-                    </Box>
+                    } />
             )})
             }
         </>
