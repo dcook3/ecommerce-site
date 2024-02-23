90668a143bbb48e7794f6ef6f115cd337876eac2
Dylan Cook <dcook@email.neit.edu>
2024-02-23 16:33:01 -0500

    Middleware functional.

---
+++ middleware/routes/products.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const productsRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../conn");
+ 
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the productss.
+productsRoutes.route("/products").get(function (req, res) {
+ let db_connect = dbo.getDb("employees");
+ db_connect
+    .collection("products")
+    .find({})
+    .toArray(function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+ 
+// This section will help you get a single products by id
+productsRoutes.route("/products/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect
+    .collection("products")
+    .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+module.exports = productsRoutes;
